9e680038467e4dde482406a094346b6c0e6df8fd
Alex Gerasimov <agerasimov@atlassian.com>
2024-07-04 21:19:49 +1000

    DCQS-431 add user credentials to extension_locust.py

--- app/extension/bamboo/extension_locust.py
+++ app/extension/bamboo/extension_locust.py
@@ -4,9 +4,8 @@ from locustio.common_utils import init_logger, bamboo_measure, run_as_specific_u
 logger = init_logger(app_type='bamboo')
 
 
-@bamboo_measure("locust_app_specific_action")
-@jira_measure("locust_generate_lighthouse_events_action")
-@run_as_specific_user(username='admin', password='XlwYc4N_:kcDfTtzkxtSaC1a')  # run as specific user
+@bamboo_measure("locust_generate_lighthouse_events_action")
+@run_as_specific_user(username='admin', password='591W2.DV225YVcCKI:CoIfrk')  # run as specific user
 def app_specific_action(locust):
     r = locust.get('/rest/lighthouse/1.0/alerts/create', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
