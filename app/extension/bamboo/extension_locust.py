9aa1ca4fcbe1cfa8559fbc800a094da9f328a02d
Alex Gerasimov <agerasimov@atlassian.com>
2024-07-05 13:14:50 +1000

    DCQS-423 created new instance and updated the credentials

--- app/extension/bamboo/extension_locust.py
+++ app/extension/bamboo/extension_locust.py
@@ -5,7 +5,7 @@ logger = init_logger(app_type='bamboo')
 
 
 @bamboo_measure("locust_generate_lighthouse_events_action")
-@run_as_specific_user(username='admin', password='591W2.DV225YVcCKI:CoIfrk')  # run as specific user
+@run_as_specific_user(username='admin', password='i2fhvn.h8NOstjtTXUHLQlrX')  # run as specific user
 def app_specific_action(locust):
     r = locust.get('/rest/lighthouse/1.0/alerts/create', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
